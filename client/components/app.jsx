1dd951b49553603368441b8af20c1d4f395fcd72
Jake Hamilton <hamiltonjm12@gmail.com>
2016-10-27 12:41:01 -0400

    Scaffold app and main components

---
+++ client/components/app.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import { Link } from 'react-router';
+import daemon from 'superagent';
+
+export default class App extends Component {
+  constructor () {
+
+  }
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
